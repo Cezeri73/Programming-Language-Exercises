5d0222cf5dc2a9ea8ed647943be4d423740a34d9
Cezeri73 <61932474+Cezeri73@users.noreply.github.com>
2023-10-23 09:53:09 +0300

    Add files via upload

---
+++ DatabaseLogger.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package polymorphisimdemo;
+
+/**
+ *
+ * @author murat
+ */
+public class DatabaseLogger extends BaseLogger {
+
+    public void Log(String message) {
+        System.out.println("Logged to Database : " + message);
+
+    }
+
+}
